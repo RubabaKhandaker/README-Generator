6ec284fbb1fa37aaa4b5fe7fde1937f3838fa26b
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2024-01-08 19:37:49 +1100

    Added more code to index.js prompts

--- index.js
+++ index.js
@@ -45,10 +45,18 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err?
+        console.error(err): console.log('Success!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data){
+        writeToFile(`./utils/README.md`, generateMarkdown(data));
+    })
+}
 
 // Function call to initialize app
 init();
