dbb700a0f6f1dcf2ee71b5960c5e5d51777416ff
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2024-01-08 18:56:49 +1100

    Added required packages and some questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        message: 'Give your project a name!',
+        name: 'Title',
+        default: 'Project 1',
+    },
+
+    {
+        type: 'input',
+        message: 'Write a brief description of your project!',
+        name: 'Description',
+        default: 'N/A'
+    },
+
+    {
+        type: 'input',
+        message: 'Provide installation instructions for your application!',
+        name: 'Installation',
+        default: 'N/A'
+    },
+
+    {
+        type: 'input',
+        message: 'Provide user instructions for this application!',
+        name: 'Usage',
+        default: 'N/A'
+    },
+
+    {
+        type: 'input',
+        message: 'Credit your contributers here!',
+        name: 'Credits & Acknowledgments',
+        default: 'N/A'
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
